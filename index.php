e28d04c21ff43eb78c95251d9c3c5071fae9125e
lucas wurtz <lucaswurtz0@gmail.com>
2022-10-31 20:43:31 +0100

    fin du projet

--- index.php
+++ index.php
@@ -7,31 +7,12 @@
 </head>
 
 <body>
-    <header class="p-10">
-        <h1 class="p-5 m-10">Tous savoir sur l'univer des livres Harry Potter</h1>
-        <div class="upNav">
-            <h3 class="m-5">Les personnes dans tous leurs détails et les Sorts</h3>
-            <img src="./public/assets/img/blazon_hogwarts.jpg" alt="Blazon de Hogwarts">
-            <div id="menuBurger">
-                <span id="spanUn"></span>
-                <span id="spanDeux"></span>
-                <span id="spanNotSeen"></span>
-            </div>
-        </div>
-        <nav id="nav" class="overflow">
-            <ul>
-                <li class="<?= $_SERVER['REQUEST_URI'] === '/' ? 'active' : ' ' ?>">
-                    <a href="#">Les différentes maisons</a>
-                </li>
-                <li class="<?= $_SERVER['REQUEST_URI'] === '/' ? 'active' : ' ' ?>">
-                    <a href="#">Tout les Personnages</a>
-                </li>
-                <li class="<?= $_SERVER['REQUEST_URI'] === '/' ? 'active' : ' ' ?>">
-                    <a href="#">Tout les Sorts</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+
+    <?php include_once "./public/include/header.php" ?>
+
+    <?php include_once "./public/include/main.php" ?>
+
+    <?php include_once "./public/include/footer.php" ?>
 </body>
 
 </html>
